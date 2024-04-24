851d564f70d7ed6bf9324e5594ccdeb71cd40700
Reyn Yamamoto <160263670+reynyamamoto@users.noreply.github.com>
2024-04-24 12:50:22 -0700

    trying to fix animated sprite

--- sprites.py
+++ sprites.py
@@ -6,8 +6,27 @@
 import pygame as pg
 from settings import *
 import sys
+from os import path
 
 vec = pg.math.Vector2
+
+SPRITESHEET = "theBell.png"
+game_folder = path.dirname(__file__)
+img_folder = path.join(game_folder, 'images')
+
+class Spritesheet:
+    # utility class for loading and parsing spritesheets
+    def __init__(self, filename):
+        self.spritesheet = pg.image.load(filename).convert()
+
+    def get_image(self, x, y, width, height):
+        # grab an image out of a larger spritesheet
+        image = pg.Surface((width, height))
+        image.blit(self.spritesheet, (0, 0), (x, y, width, height))
+        # image = pg.transform.scale(image, (width, height))
+        image = pg.transform.scale(image, (width * 1, height * 1))
+        return image
+
 
 # write a player class
 class Player(pg.sprite.Sprite):
@@ -15,8 +34,15 @@ class Player(pg.sprite.Sprite):
         self.groups = game.all_sprites
         pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
-        self.image = pg.Surface((TILESIZE, TILESIZE))
-        self.image = game.player_img
+        #self.image = pg.Surface((TILESIZE, TILESIZE))
+        #self.image = game.player_img
+        self.spritesheet = Spritesheet(path.join(img_folder, SPRITESHEET))
+        self.load_images()
+        self.image = self.standing_frames[0]   
+        self.last_update = 0
+        self.material = True
+        self.jumping = False
+        self.walking = False    
         #self.image.fill(GREEN)
         self.rect = self.image.get_rect()
         self.vx, self.vy = 0, 0
@@ -24,6 +50,7 @@ class Player(pg.sprite.Sprite):
         self.y = y * TILESIZE
         self.speed = 300
         self.score = 0
+
 
     def get_keys(self):
         self.vx, self.vy = 0, 0
@@ -36,9 +63,11 @@ class Player(pg.sprite.Sprite):
             self.vy = -self.speed
         if keys[pg.K_DOWN] or keys[pg.K_s]:
             self.vy = self.speed
+    
     def update(self):
         # self.rect.x = self.x
         # self.rect.y = self.y
+        self.animate()
         self.get_keys()
         self.x += self.vx * self.game.dt
         self.y += self.vy * self.game.dt
@@ -89,7 +118,32 @@ class Player(pg.sprite.Sprite):
                 elif str(hits[0].__class__.__name__) == "Spike":
                     pg.quit()
                     sys.exit()
+
+    def load_images(self):
+        self.standing_frames = [self.spritesheet.get_image(0,0, 32, 32), 
+                                self.spritesheet.get_image(32,0, 32, 32),
+
+                                ]
+        self.walking_frames = [
+                                self.spritesheet.get_image(64,0, 32, 32),
+                                self.spritesheet.get_image(96,0, 32, 32),
+        ]
     
+    def animate(self):
+        now = pg.time.get_ticks()
+        if now - self.last_update > 350:
+            self.last_update = now
+            self.current_frame = (self.current_frame + 1) % len(self.standing_frames)
+            bottom = self.rect.bottom
+            if not self.walking:
+                self.image = self.standing_frames[self.current_frame]
+            else:
+                self.image = self.walking_frames[self.current_frame]
+            self.rect = self.image.get_rect()
+            self.rect.bottom = bottom
+
+    
+
 class Wall(pg.sprite.Sprite):
     def __init__(self, game, x, y):
         self.groups = game.all_sprites, game.walls
